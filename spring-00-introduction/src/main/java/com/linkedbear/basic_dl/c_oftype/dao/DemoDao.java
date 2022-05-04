b1922bda3ba732b0c7011b2b0b8ac7c91773aedc
wychmod <545480453@qq.com>
2022-05-04 18:21:44 +0800

    依赖注入-批量查询依赖

---
+++ spring-00-introduction/src/main/java/com/linkedbear/basic_dl/c_oftype/dao/DemoDao.java
@@ -0,0 +1,4 @@
+package com.linkedbear.basic_dl.c_oftype.dao;
+
+public interface DemoDao {
+}
---
+++ spring-00-introduction/src/main/java/com/linkedbear/basic_dl/c_oftype/dao/impl/DemoOracleDao.java
@@ -0,0 +1,6 @@
+package com.linkedbear.basic_dl.c_oftype.dao.impl;
+
+import com.linkedbear.basic_dl.c_oftype.dao.DemoDao;
+
+public class DemoOracleDao implements DemoDao {
+}
